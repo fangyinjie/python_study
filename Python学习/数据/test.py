548d594caf27bb7b7131d1096b7ab98be199363d
Administrator <494675446@qq.com>
2024-02-03 15:48:04 +0800

    123

--- Python学习/数据/test.py
+++ Python学习/数据/test.py
@@ -3,11 +3,11 @@ import pandas as pd
 # data = pd.read_csv('iris.csv') #导入数据\
 # print(data.head()) #打印数据前五行，结构如下：
 # SepalLength  SepalWidth  PetalLength  PetalWidth Name0
-# 5.1         3.5          1.4         0.2     Iris-setosa1
-# 4.9         3.0          1.4         0.2    Iris-setosa2
-# 4.7         3.2          1.3         0.2     Iris-setosa3
+# 5.1         3.5          1.4         0.2      Iris-setosa1
+# 4.9         3.0          1.4         0.2      Iris-setosa2
+# 4.7         3.2          1.3         0.2      Iris-setosa3
 # 4.6         3.1          1.5         0.2      Iris-setosa4
-# 5.0         3.6          1.4         0.2     Iris-setosa
+# 5.0         3.6          1.4         0.2      Iris-setosa
 
 
 import matplotlib.pyplot as plt
