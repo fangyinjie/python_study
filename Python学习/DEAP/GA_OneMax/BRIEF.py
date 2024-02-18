488a24963965938d49d2a210fd492f103e3172a4
Administrator <494675446@qq.com>
2024-02-18 08:55:59 +0800

    123

--- Python学习/DEAP/GA_OneMax/BRIEF.py
+++ Python学习/DEAP/GA_OneMax/BRIEF.py
@@ -3,12 +3,13 @@ from deap import creator
 from deap import tools
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 import time
 
 P_MUTATION = 0.1  # 变异的概率
 P_CROSSOVER = 0.9  # 交叉的概率
 MAX_GENERATION = 500  # 停止条件中的最大迭代次数
-ONE_MAX_LENGTH = 100  # 优化的bit字符串的长度
+ONE_MAX_LENGTH = 20  # 优化的bit字符串的长度
 POPULATION_SIZE = 200  # 种群中的个体数量(越大越平滑)
 
 
@@ -23,7 +24,8 @@ def time_val(dec):
     return time_inner
 
 def oneMaxFitness(individual):
-    return sum(individual),
+    matrix = np.array(individual).reshape((ONE_MAX_LENGTH, ONE_MAX_LENGTH))
+    return np.sum(matrix),
 
 @time_val
 def main():
@@ -37,7 +39,8 @@ def main():
     meanFitnessValues = []      # 平均适应度
 
     Gen_Counter = 0
-    while max(fitnessValues) < ONE_MAX_LENGTH and (Gen_Counter := Gen_Counter + 1) < MAX_GENERATION:
+    # np.max
+    while max(fitnessValues) < ONE_MAX_LENGTH * ONE_MAX_LENGTH and (Gen_Counter := Gen_Counter + 1) < MAX_GENERATION:
         offspring = toolbox.select(population, len(population))   # 遗传运算符1——selection运算符（toolbox.select定义的锦标赛选择），将物种及其长度作为参数传递给选择运算符
         offspring = list(map(toolbox.clone, offspring))
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
@@ -82,16 +85,21 @@ if __name__ == "__main__":
     # (1) Obj create
     creator.create("FitnessMax", base.Fitness, weights=(1.0,))
     creator.create("Individual", list, fitness=creator.FitnessMax)
+    # creator.create("Individual", np.ndarray, fitness=creator.FitnessMax)  # 定义个体类型
 
     # (2) Tool create
     toolbox = base.Toolbox()  # 定义toolbox变量
     toolbox.register("zeroOrOne", random.randint, 0, 1)  # 注册zeroOrOne运算
-    toolbox.register("individualCreator", tools.initRepeat, creator.Individual,  # (1) 将creator.Individual类作为放置结果对象的容器类型
-                     toolbox.zeroOrOne,  # (2) zeroOrOne操作是生成对象的函数
-                     ONE_MAX_LENGTH)  # (3) 常量ONE_MAX_LENGTH作为要生成的对象数目
+    # toolbox.register("zeroOrOne", np.random.choice, a=[1, 0], size=ONE_MAX_LENGTH)  # 注册zeroOrOne运算
+    toolbox.register("individualCreator", tools.initRepeat,
+                     creator.Individual,                       # (1) 将creator.Individual类作为放置结果对象的容器类型
+                     toolbox.zeroOrOne,                              # (2) zeroOrOne操作是生成对象的函数
+                     ONE_MAX_LENGTH * ONE_MAX_LENGTH)                # (3) 常量ONE_MAX_LENGTH作为要生成的对象数目
 
     toolbox.register("populationCreator", tools.initRepeat, list,  # (1) 将列表类作为容器类型
                      toolbox.individualCreator)  # (2) 用于生成列表中对象的函数——personalCreator运算符
+    # toolbox.register("populationCreator", tools.initRepeat, list,  # (1) 将列表类作为容器类型
+    #                  toolbox.zeroOrOne)  # (2) 用于生成列表中对象的函数——personalCreator运算符
     toolbox.register("evaluate", oneMaxFitness)
 
     toolbox.register("select", tools.selTournament, tournsize=3)  # (1) 创建遗传算子1-选择运算符;
